d26e05eb7db0769291a744fc1d1259a3fe9f2121
mahmoud khudairi <mahmoudkhudairi@gmail.com>
2017-05-29 23:34:20 +0800

    Removing the segue from left from storyboard due to some bugs related

--- TinderClone/User.swift
+++ TinderClone/User.swift
@@ -8,19 +8,6 @@
 
 import UIKit
 
-//class User {
-//  var id: String?
-//  var name: String?
-//  var email: String?
-//  var profileImageUrl: String?
-//  
-//  init(dictionary: [String: Any]) {
-//    self.id = dictionary["uid"] as? String
-//    self.name = dictionary["name"] as? String
-//    self.email = dictionary["email"] as? String
-//    self.profileImageUrl = dictionary["profileImageUrl"] as? String
-//  }
-//}
 class User {
   var id: String?
   var name: String?
